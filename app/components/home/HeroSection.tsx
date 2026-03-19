39ef40ee7c6399a7141fb355cc699d3b97aba9b1
JohanNithishTCP <tcp.johannithish@google.com>
2026-03-19 11:46:44 +0530

    banner text animation delay

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -36,14 +36,14 @@ export function HeroSection() {
         <div className="max-w-7xl mx-auto text-center text-white">
           <h1
             data-aos="fade-up"
-            data-aos-delay="200"
+            data-aos-delay="3000"
             className="text-2xl sm:text-3xl md:text-5xl lg:text-[35px] font-bold mb-4 md:mb-6 leading-tight [text-shadow:2px_2px_10px_rgba(0,0,0,0.5)]"
           >
             Precision Medical Technology You Can Trust
           </h1>
           <p
             data-aos="fade-up"
-            data-aos-delay="400"
+            data-aos-delay="3000"
             className="text-sm sm:text-base md:text-lg lg:text-[19px] leading-relaxed max-w-5xl mx-auto [text-shadow:1px_1px_5px_rgba(0,0,0,0.5)] opacity-90"
           >
             We deliver high-quality medical devices engineered with precision and reliability. Our solutions are designed to support healthcare professionals with safety, efficiency, and uncompromising performance across every application.
